4b60c999a6e84da52eb0b8a1cb32387960725dd1
ababoescu <ababoescu@ucsd.edu>
2023-01-29 21:27:24 -0800

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -5,27 +5,29 @@ import java.util.*;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    //Scanner stringS = new Scanner(System.in);
+
+    ArrayList <String> myStringList = new ArrayList<>();
     String s = new String();
     int num = 0;
-
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Ana Maria's Number: %d", num);
-        } //else if (url.getPath().equals("/add-message"))
+            return "Need to add this to your url code: /add-message?s=";
+        } 
         else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    for(int i = 1; i < parameters.length; i++){
-                        //s = parameters[i];
-                        return String.format("%s\n", parameters[i]);
+                if (parameters[0].equals("s")) {    
+                    s = parameters[1];
+                    myStringList.add(s);
+
+                    String tempConcatString = myStringList.get(0);
+                    tempConcatString = tempConcatString + "\n";
+                    for(int i = 1; i < myStringList.size(); i++){
+                        tempConcatString = tempConcatString + myStringList.get(i);
+                        tempConcatString = tempConcatString + "\n";
                     }
-                    //s = parameters[1];
-                    
-                    //return String.format("your word is = %s\n", parameters[1]);
-                    
+                    return tempConcatString; 
                 }
             }
             return "404 Not Found!";
@@ -39,9 +41,7 @@ class StringServer {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new Handler());
     }
 }
